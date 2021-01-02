24a2eb3fad4ef6d728d2b1cd703d1c2fdb2a0229
Jesse Spielman <jesse.spielman@gmail.com>
2021-01-02 15:51:46 +0000

    visualbell patch

--- x.c
+++ x.c
@@ -254,6 +254,7 @@ static char *opt_name  = NULL;
 static char *opt_title = NULL;
 
 static int oldbutton = 3; /* button event on startup: 3 = release */
+static int bellon = 0;    /* visual bell status */
 
 void
 clipcopy(const Arg *dummy)
@@ -1733,6 +1734,8 @@ xbell(void)
 		xseturgency(1);
 	if (bellvolume)
 		XkbBell(xw.dpy, xw.win, bellvolume, (Atom)NULL);
+	if (!bellon) /* turn visual bell on */
+		bellon = 1;
 }
 
 void
@@ -1978,7 +1981,13 @@ run(void)
 			}
 		}
 
-		draw();
+		if (bellon) {
+			bellon++;
+			bellon %= 3;
+			MODBIT(win.mode, !IS_SET(MODE_REVERSE), MODE_REVERSE);
+			redraw();
+		}
+		else draw();
 		XFlush(xw.dpy);
 		drawing = 0;
 	}
